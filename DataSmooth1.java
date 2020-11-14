217edda9c815450cf180e1906cd0f7061041ba7f
gray rahm <53866989+grayrahm@users.noreply.github.com>
2020-11-14 17:51:11 -0500

    Update DataSmooth1.java

--- DataSmooth1.java
+++ DataSmooth1.java
@@ -1,36 +1,33 @@
 import java.util.LinkedList;
 
 class DataSmooth1 {
-  DataSmooth1(){}
-  
-  public LinkedList<Double> dataSmooth(LinkedList<Show> shows) 
-  {
-      LinkedList<Double> averageData = new LinkedList<>();
-      LinkedList<Double> smoothData = new LinkedList<>();
+    DataSmooth1(){}
 
-      for(Show show: shows) {
-          double runTime = 0;
-          for (Episode episode : show.episodes) {
-              runTime = runTime + episode.runTime;
+    /**
+     * takes a list of shows and smooths the runtimes of the episodes
+     * @param shows a list of shows
+     * @return the smooth list
+     */
+    public LinkedList<Double> dataSmooth(LinkedList<Show> shows)
+    {
+        LinkedList<Double> averagedData = new LinkedList<>();
+        LinkedList<Double> smoothedData = new LinkedList<>();
 
-          }
-          double averageRunTime = runTime / (show.episodes.size());
-          averageData.add(averageRunTime);
-
-      }
-
-      if (averageData.size() > 0) {
-          smoothData.add(averageData.getFirst());
-
-          for(int i = 1; i<(averageData.getFirst()-1);i++) {
-              smoothData.add((averageData.get(i) + averageData.get(i-1))/3);
-          }
-          smoothData.add(averageData.getLast());
-
-
-      }
-      return smoothData;
-
-
-  }
+        for(Show show: shows) {
+            double runTime = 0;
+            for(Episode episode: show.episodes) {
+                runTime = runTime + episode.runTime;
+            }
+            double avgRunTime = runTime/(show.episodes.size());
+            averagedData.add(avgRunTime);
+        }
+        if (averagedData.size()>0) {
+            smoothedData.add(averagedData.getFirst());
+            for(int i = 1; i<(averagedData.size()-1);i++) {
+                smoothedData.add((averagedData.get(i)+averagedData.get(i-1)+averagedData.get(i+1))/3);
+            }
+            smoothedData.add(averagedData.getLast());
+        }
+        return smoothedData;
+    }
 }
